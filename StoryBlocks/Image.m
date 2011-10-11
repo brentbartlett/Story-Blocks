9b9d9ba14229c9f4b044c33440b2d0641720b681
Patrick Tescher <pat2man@gmail.com>
2011-10-11 14:40:56 -0700

    Include a few more things

---
+++ StoryBlocks/Image.m
@@ -0,0 +1,17 @@
+//
+//  Image.m
+//  StoryBlocks
+//
+//  Created by Patrick Tescher on 10/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Image.h"
+
+
+@implementation Image
+
+@dynamic imageName;
+@dynamic stories;
+
+@end
